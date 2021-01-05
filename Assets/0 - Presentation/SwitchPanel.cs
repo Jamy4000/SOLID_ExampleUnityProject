0d1f29dacb0b4879823ad027b3f75bc57d1b10b3
Arnaud Briche <arnaud.briche@ubisoft.com>
2021-01-05 14:37:44 +0100

    Finished presentation background

---
+++ Assets/0 - Presentation/SwitchPanel.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Solid.Presentation
+{
+    public class SwitchPanel : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] _panels;
+
+        [SerializeField]
+        private string _nextSceneName = "1.1 - Bad_SRP";
+
+        private int _currentPanelIndex = 0;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                if (_currentPanelIndex == 0)
+                    return;
+
+                _panels[_currentPanelIndex].SetActive(false);
+                _currentPanelIndex--;
+                _panels[_currentPanelIndex].SetActive(true);
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                if (_currentPanelIndex == _panels.Length - 1)
+                    return;
+
+                _panels[_currentPanelIndex].SetActive(false);
+                _currentPanelIndex++;
+                _panels[_currentPanelIndex].SetActive(true);
+            }
+            else if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene(_nextSceneName);
+            }
+        }
+    }
+}
